c93524b151c8a1cf379fbcaced423472521ad65b
ahadjawaid <ahadjawaid0@gmail.com>
2025-01-25 16:56:59 -0600

    feat: added init for model

--- HackTAMU25App/HackTAMU25App/HackTAMU25AppApp.swift
+++ HackTAMU25App/HackTAMU25App/HackTAMU25AppApp.swift
@@ -6,12 +6,30 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct HackTAMU25AppApp: App {
+//    var sharedModelContainer: ModelContainer = initializeModelContainer()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+//        .modelContainer(SampleData.shared.modelContainer)
+    }
+}
+
+func initializeModelContainer() -> ModelContainer {
+    let schema = Schema([
+        Topic.self,
+    ])
+    
+    let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: true)
+
+    do {
+        return try ModelContainer(for: schema, configurations: [modelConfiguration])
+    } catch {
+        fatalError("Could not create ModelContainer: \(error)")
     }
 }
