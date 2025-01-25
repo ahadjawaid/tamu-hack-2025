771c57a8941eb750d00bd18c30c7789e6f98c7bb
ahadjawaid <ahadjawaid0@gmail.com>
2025-01-25 12:13:55 -0600

    feat: initalized website

---
+++ HackTAMU25App/HackTAMU25App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HackTAMU25App
+//
+//  Created by tk on 1/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HackTAMU25App/HackTAMU25App/HackTAMU25AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackTAMU25AppApp.swift
+//  HackTAMU25App
+//
+//  Created by tk on 1/25/25.
+//
+
+import SwiftUI
+
+@main
+struct HackTAMU25AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
