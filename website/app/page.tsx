a67fd17e74f2c4e35c657e1f3ed25f7ee675721e
ahadjawaid <ahadjawaid0@gmail.com>
2025-01-26 01:18:59 -0600

    feat: added description

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -1,101 +1,130 @@
-import Image from "next/image";
+"use client"
+import Image from 'next/image';
+import { useState } from 'react';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { useToast } from "@/hooks/use-toast"
+import { CheckCircle, XCircle, Users } from 'lucide-react';
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [email, setEmail] = useState('');
+  const { toast } = useToast()
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const googleFormAction = 'https://docs.google.com/forms/d/e/1FAIpQLSfLZIK2VP_JimUlGZiwnsiyP7GbfdAElBvdC3fKk1mib6pCsA/formResponse';
+  const emailFieldName = 'entry.142330486';
+  const earlySupports = 0;
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+    try {
+      await fetch(googleFormAction, {
+        method: 'POST',
+        mode: 'no-cors',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: new URLSearchParams({ [emailFieldName]: email }),
+      });
+
+      toast({
+        title: "Success!",
+        description: "You have been added to the waitlist!",
+        variant: "default",
+        className: "bg-white text-black",
+        action: <CheckCircle className="w-5 h-5 text-green-500" />,
+      })
+
+      setEmail('');
+    } catch (error) {
+      toast({
+        title: "Error",
+        description: "An error occurred. Please try again.",
+        variant: "destructive",
+        className: "bg-white border-red-500 text-black",
+        action: <XCircle className="w-5 h-5 text-red-500" />,
+      })
+
+      console.error('Error submitting form:', error);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  return (
+    <div className='relative min-h-screen'>
+      <Image 
+        src="/hero-bg.svg"
+        alt="Background"
+        width={1920}
+        height={1080}
+        className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 opacity-80"
+        priority
+      />
+      
+      <div className='relative min-h-screen max-w-7xl mx-auto px-4 flex flex-col justify-between'>
+        <div className='flex flex-col sm:gap-y-18 md:gap-y-22'>
+          <nav className='py-8 sm:py-12 md:py-16'>
+            <Image src="/logo.svg" alt="Logo" width={150} height={50} />
+          </nav>
+
+          <main className='flex flex-col gap-y-24 sm:flex-row justify-between items-center'>
+            <div className="flex flex-col gap-y-8 max-w-2xl">
+              <div className='w-full'>
+                <h1 className="mb-4 sm:mb-6 text-2xl sm:text-3xl md:text-4xl font-bold leading-tight">
+                  Where music meets knoweldge.
+                </h1>
+                <p className="text-md sm:text-lg md:text-xl text-zinc-400">
+                  Make learning engaging with custom generated songs pertaining to different educational subjects
+                </p>
+              </div>
+
+              <div className='block md:hidden mx-auto'>
+                <Image src="/hero.png" alt="Hero" width={200} height={200} priority />
+              </div>
+
+              <form className='flex flex-col gap-y-6' onSubmit={handleSubmit}>
+                <div className="space-y-3">
+                  <div className="flex flex-col sm:flex-row gap-x-6 gap-y-3">
+                    <div className="flex-1">
+                      <label className="sr-only">Email</label>
+                      <Input
+                        className="bg-white text-black h-12"
+                        type="email"
+                        placeholder="name@email.com"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                      />
+                    </div>
+
+                    <Button
+                      type="submit"
+                      disabled={isSubmitting}
+                      className='bg-yellow-400 hover:bg-yellow-500 h-12 text-black px-8 font-medium text-sm sm:text-md md:text-lg'
+                    >
+                      {isSubmitting ? 'Submitting...' : 'Join Waitlist'}
+                    </Button>
+                  </div>
+                  <div className="flex items-center justify-center sm:justify-start gap-x-2 text-zinc-500 text-sm sm:text-base">
+                    <Users className="w-4 h-4" />
+                    <span>{earlySupports.toLocaleString()} early supporters (updates daily)</span>
+                  </div>
+                </div>
+              </form>
+            </div>
+
+            <div className='hidden md:block'>
+              <Image
+                src="/hero.png"
+                alt="Hero"
+                width={500}
+                height={500}
+                className="drop-shadow-2xl"
+                priority
+              />
+            </div>
+          </main>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
